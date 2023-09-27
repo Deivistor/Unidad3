2ea20767af5958b0d3e8fbb3b4cdfcff39d4c301
Deivistor <143622024+Deivistor@users.noreply.github.com>
2023-09-26 18:08:05 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package practical.pkgif.statement_o;
+import java.util.Scanner;
+
+
+// The main method must be in a class named "Main".
+public class PracticaIfStatement O {
+    public static void main(String[] args)  {
+        Scanner lectura = new Scanner(source:System.in);
+        int numero;
+        System.out.println(x: "Ingresa un numero");
+        numero = lectura.nextInt();
+
+        if (numero % 2 == 0)
+            {
+                System.out.println(x: "El numero ingresado es par");
+                }
+        else
+        {
+                System.out.println(x: "El numero ingresado es impar");
+            }
+        lectura.close();
+        }
+        
+            
+
+
+        
+
+
+                              
+    }
+}
